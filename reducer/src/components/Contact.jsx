9730af65480324ef3abdc822c526e98373a6e484
Kennedy <kennedy.gamarra@hypersoft.co>
2022-10-03 16:50:34 -0500

    Add toggle new contact form

--- reducer/src/components/Contact.jsx
+++ reducer/src/components/Contact.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useReducer } from "react";
+import { useEffect ,useReducer ,useState} from "react";
 import { ContactsReducer } from "../reducers/ContactsReducer";
 import ContactTable from "./ContactTable";
 import FormAdd from "./FormAdd";
@@ -15,10 +14,15 @@ const Contact = () => {
   useEffect(() => {
     localStorage.setItem("contacts", JSON.stringify(state));
   }, [state])
+
+  const [formView, setFormView] = useState(false);
   
   return (
     <div className="container mt-3">
-      <FormAdd dispatch={dispatch} />
+      <button onClick={()=> setFormView(!formView)} className="btn btn-primary">
+        {!formView ? 'Add contact' : 'Close'}
+      </button>
+      {formView &&  <FormAdd dispatch={dispatch} />}
       <ContactTable contacts={state} dispatch={dispatch} />
     </div>
   );
